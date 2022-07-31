59befca0eeae0fbb512dee20d5237019f40d372d
octavia <Ogomes21@gmail.com>
2022-07-31 01:47:17 -0400

    Added Tshirt properties.

--- src/main/java/models/Hat.java
+++ src/main/java/models/Hat.java
@@ -3,7 +3,8 @@ package models;
 public class Hat {
     private String color;
     private String brand;
-    private int qty;
     private int size;
     private float price;
+    private int qty;
+
 }
--- src/main/java/models/Tshirt.java
+++ src/main/java/models/Tshirt.java
@@ -1,4 +1,9 @@
 package models;
 
 public class Tshirt {
+    private String color;
+    private String brand;
+    private int size;
+    private float price;
+    private int qty;
 }
